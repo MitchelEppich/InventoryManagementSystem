2dd55d2db4e65811e1873e89ef13ccd4d62a5838
FhelipeTurati <fmturati@gmail.com>
2019-05-01 14:43:02 -0700

    Revert "before merging with my friends:"
    This reverts commit a5e2176d036c5f4c9e233524278d6f4a8275e678.

--- components/Logs/logItem.js
+++ components/Logs/logItem.js
@@ -1,4 +1,4 @@
-import moment from "moment";
+import moment from "moment"
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -9,23 +9,16 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 const item = props => {
+
+
+
   return (
-    <div className="h-auto w-full flex justify-between bg-white border border-1 border-grey-lightest hover:border-teal-light">
-      <div className="h-full w-1/6 pt-2 text-sm text-crimson text-center">
-        {props.index + 1}
-      </div>
-      <div className="h-full w-1/6 py-2 text-sm text-crimson text-center">
-        {props.name}
-      </div>
-      <div className="h-full w-1/3 py-2 text-sm text-crimson text-center">
-        {props.action}
-      </div>
-      <div className="h-full w-1/6 py-2 px-8 text-sm text-crimson text-left">
-        {props.machine}
-      </div>
-      <div className="h-full w-1/6 py-2 text-sm text-crimson text-center">
-        {moment(props.date).format("MMM Do YY, H:mm:ss")}
-      </div>
+    <div  className="h-auto w-full flex justify-between bg-teal-dark border border-1 border-white hover:border-salmon-light" >
+    <div className="h-full w-1/6 pt-2 text-sm text-crimson text-center">{props.index + 1}</div>
+        <div className="h-full w-1/6 py-2 text-sm text-crimson text-center">{props.name}</div> 
+        <div className="h-full w-1/3 py-2 text-sm text-crimson text-center">{props.action}</div> 
+        <div className="h-full w-1/6 py-2 px-8 text-sm text-crimson text-left">{props.machine}</div>
+        <div className="h-full w-1/6 py-2 text-sm text-crimson text-center">{moment(props.date).format("MMM Do YY, H:mm:ss")}</div> 
     </div>
   );
 };
